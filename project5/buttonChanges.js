07afa6527dd34bb729304b2904ed273daaf5dc4c
Sergio Castanon <sergio.castanon4@gmail.com>
2016-07-20 16:03:25 +0000

    Added the changing button project

---
+++ project5/buttonChanges.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    var background = document.querySelector('.back');
+    background.classList.toggle('color');    
+}
+
+function addClass() {
+    var button = document.querySelector(".rotate");
+    button.classList.toggle("move");
+}
