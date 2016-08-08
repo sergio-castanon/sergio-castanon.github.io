015f540fe823e1dd8dc67560d7fd2fc69374cbdc
Sergio Castanon <sergio.castanon4@gmail.com>
2016-08-08 04:24:48 +0000

    Fixed window issue

--- project2/moving.js
+++ project2/moving.js
@@ -1,15 +1,28 @@
 // http://www.w3schools.com/cssref/css3_pr_transform.asp
-var height = window.innerHeight;
-var width = window.innerWidth;
-
 var move = function() {
     var button = document.getElementById('mainButton');
     var number = randNum();
     var secondNum = randNum();
-    button.style.transform = "translate(" + number + "px, " + secondNum + "px)";
+    var numbers = checkEdge(number, secondNum);
+    button.style.transform = "translate(" + numbers[0] + "px, " + numbers[1] + "px)";
 };
 
 function randNum() {
     var num = ((Math.random() * 1000) + 1);
     return num;
+}
+
+function checkEdge(number, secondNum) {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    var buttonHeight = document.getElementById("mainButton").style.height;
+    if ((height - buttonHeight) <= secondNum) {
+        if (width <= number) {
+            return [100, 200];
+        }
+        return [number, 200];
+    }
+    else {
+        return [number, secondNum];
+    }
 }
